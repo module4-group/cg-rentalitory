8fea1b2059f8d929482d107885b75aa337393d9c
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2023-11-29 02:55:59 +0700

    adding security config for ADMIN and non-ADMIN

--- src/main/java/com/codegym/bemd4/controller/AuthController.java
+++ src/main/java/com/codegym/bemd4/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.codegym.bemd4.controller;
 
 import com.codegym.bemd4.model.dto.request.LoginRequestDTO;
 import com.codegym.bemd4.model.dto.response.LoginResponseDTO;
+import com.codegym.bemd4.model.entity.person.Role;
 import com.codegym.bemd4.security.JwtTokenProvider;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(value = "*")
 @RestController
@@ -38,10 +44,18 @@ public class AuthController {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             String token = tokenProvider.generateToken(authentication);
-            return new ResponseEntity<>(new LoginResponseDTO("Đăng nhập thành công!", token), HttpStatus.OK);
+            boolean isAdmin=false;
+
+            List<String> roles = authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+            if (roles.contains("ROLE_ADMIN")){
+                isAdmin=true;
+            }
+            return new ResponseEntity<>(new LoginResponseDTO("Đăng nhập thành công!", token,isAdmin), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(new LoginResponseDTO("Đăng nhập thất bại!", null), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new LoginResponseDTO("Đăng nhập thất bại!", null,false), HttpStatus.BAD_REQUEST);
         }
 
     }
